f7e49af8448b4d8bf1392dfc8703ae05895ee107
Mustafa <mustafke@stud.ntnu.no>
2025-04-07 16:30:03 +0200

    Fixed issue where first entry in registrations endpoint wouldn't have its id, also renamed registraion in model to registraionStruct

--- constants/errorMessages/errorMessages.go
+++ constants/errorMessages/errorMessages.go
@@ -1,19 +1,17 @@
 package errorMessages
 
+// Error messages for the API
+
 const (
 	MethodNotAllowed = "Attempted method not allowed"
+	FirestoreError   = "Could not store to Firestore: "
 
-	InvalidJSON = "Invalid JSON data"
-
-	FirestoreError = "Could not store to Firestore: "
-
+	InvalidJSON           = "Invalid JSON data"
 	InvalidRegistrationID = "Invalid registration ID"
 
-	RegisterNotFound = "No register found with given ID "
-
 	DeleteError = "Could not delete registration: "
-
 	UpdateError = "Could not update registration: "
 
-	NoIDProvided = "No ID provided in the request"
+	NoIDProvided     = "No ID provided in the request"
+	RegisterNotFound = "No register found with given ID "
 )
