2e2fe86800da6e3fa8bb399b425ca813cee65adc
augusjo <augusjo@stud.ntnu.no>
2025-04-09 00:32:32 +0200

    sync

--- constants/constants.go
+++ constants/constants.go
@@ -5,15 +5,19 @@ const (
 	BaseAPI = "/dashboard/v1"
 
 	// Local endpoints
-
 	Registrations = BaseAPI + "/registrations/"
 	Dashboards    = BaseAPI + "/dashboards/"
 	Notifications = BaseAPI + "/notifications/"
 	Status        = BaseAPI + "/status/"
 
 	// External endpoints
-
 	RestCountriesAPI = "http://129.241.150.113:8080/v3.1"
 	OpenMeteoAPI     = "https://api.open-meteo.com/v1/forecast"
 	CurrencyAPI      = "http://129.241.150.113:9090/currency/"
+
+	// webhook event constants
+	EventRegister = "REGISTER"
+	EventChange   = "CHANGE"
+	EventDelete   = "DELETE"
+	EventInvoke   = "INVOKE"
 )
